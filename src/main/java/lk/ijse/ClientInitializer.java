59087fe5401a55b74a249cc128a7900849acc826
nimashidewanmini <nimashidewanmini@gmail.com>
2024-01-19 16:54:13 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/ClientInitializer.java
@@ -0,0 +1,20 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Client.fxml"))));
+        stage.show();
+
+    }
+}
---
+++ src/main/java/lk/ijse/ClientInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class ClientInitializerWrapper {
+    public static void main(String[] args) {
+        ClientInitializer.main(args);
+    }
+}
